bbb3833808b5f97361af02f6f3c3358126d45706
WillGAndre <xunga@mail.com>
2021-12-18 12:09:31 +0000

    Added unsecure file upload mechanism

--- sr_web_vuln/app/routes.py
+++ sr_web_vuln/app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, flash, redirect, url_for
+import flask
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user
@@ -6,6 +7,7 @@ from app.models import User
 from flask_login import logout_user, login_required
 from flask import request
 from werkzeug.urls import url_parse
+import os
 # import requests
 # import json
 
@@ -66,6 +68,8 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
+
+# Profile page
 @app.route('/user/<username>')
 @login_required
 def user(username):
@@ -74,4 +78,40 @@ def user(username):
         {'author': user, 'title': 'Test title post #1', 'body': 'Test post #1'},
         {'author': user, 'title': 'Test title post #2', 'body': 'Test post #2'}
     ]
+    filename = os.path.join(app.config['UPLOAD_FOLDER'], username)
+    if (os.path.exists(filename)):
+        return render_template('user.html', user=user, posts=posts, filename=username)
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/upload')
+@login_required
+def upload():
+    return render_template('upload_file.html')
+
+
+@app.route('/uploader', methods = ['POST'])
+@login_required
+def uploader():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+    if file.filename == "":
+        return redirect(request.url)
+
+    if file:
+        # filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], current_user.username))
+        posts = [
+            {'author': user, 'title': 'Test title post #1', 'body': 'Test post #1'},
+            {'author': user, 'title': 'Test title post #2', 'body': 'Test post #2'}
+        ]
+        return render_template('user.html', user=current_user, posts=posts, filename=current_user.username)
+    else:
+        return redirect(request.url)
+
+
+@app.route('/display/<filename>')
+def display(filename):
+    return redirect(url_for('static', filename='uploads/'+filename), code=301)
